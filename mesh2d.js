df5e0e731c80667f4ef019ba291fee15151f054e
Tentone <tentone@outlook.com>
2018-08-19 00:59:44 +0100

    Gaps filled

---
+++ mesh2d.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//Check if three.js is included
+if(window.THREE === undefined)
+{
+	console.error("THREE.Mesh2D: three.js is required for this library to work.");
+}
+
+function Mesh2D(img)
+{
+
+}
